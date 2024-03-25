08b4a1736fa895892de241b506c7d6f38d0722db
YooHeej <sprite1139@gmail.com>
2024-03-25 11:53:33 +0900

    03251153

--- src/main/java/com/example/springSecurity/config/SecurityConfig.java
+++ src/main/java/com/example/springSecurity/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import jakarta.servlet.DispatcherType;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -14,7 +16,22 @@ public class SecurityConfig {
 	public SecurityFilterChain filterChanin(HttpSecurity http) throws Exception {
 		http.csrf(auth -> auth.disable())					// 괄호 안에 람다함수를 사용해야 함
 			.headers(x -> x.frameOptions(y -> y.disable()))			// CK Editor image upload
-		;
+			.authorizeHttpRequests(auth -> auth
+					.dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+					.requestMatchers("/user/login", "/user/logout", "/user/register",
+							"/img/**", "/css/**", "/js/**", "/error/**").permitAll()
+					.requestMatchers("/admin/**").hasAuthority("ADMIN")
+					.anyRequest().authenticated()
+				)
+				.formLogin(auth -> auth
+						.loginPage("/user/login")				// 로그인 폼
+						.loginProcessingUrl("/user/login")		// 스프링 시큐리티가 만듬. UserDetailsService 구현객체에서 처리해줘야 함
+						.usernameParameter("uid")
+						.passwordParameter("pwd")
+						.defaultSuccessUrl("/user/loginSuccess", true)		// 내가 로그인 후 해야할 일, ex) 세션 세팅, '오늘의 메시지' 등등
+						.permitAll()
+				)
+			;
 		
 		return http.build();
 	}
