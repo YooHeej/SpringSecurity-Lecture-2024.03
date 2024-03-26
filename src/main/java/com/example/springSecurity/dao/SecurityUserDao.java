d4eaf07081d85f6dede981659fbf2762be1ca6e9
YooHeej <sprite1139@gmail.com>
2024-03-26 16:02:17 +0900

    03261602

--- src/main/java/com/example/springSecurity/dao/SecurityUserDao.java
+++ src/main/java/com/example/springSecurity/dao/SecurityUserDao.java
@@ -15,18 +15,18 @@ public interface SecurityUserDao {
 	@Select("select * from securityUser where uid=#{uid}")
 	SecurityUser getUserByUid(String uid);
 	
-	@Select("select * from securityUser where isDelete=0 order by regDate desc"
+	@Select("select * from securityUser where isDeleted=0 order by regDate desc"
 			+ " limit #{count} offset #{offset}")
 	List<SecurityUser> getSecurityUserList(int count, int offset);
 	
-	@Select("select count(uid) from securityUser where isDelete=0")
+	@Select("select count(uid) from securityUser where isDeleted=0")
 	int getSecurityUserCount();
 	
 	@Insert("insert into securityUser values (#{uid}, #{pwd}, #{uname}, #{email},"
 			+ " default, default, #{picture}, #{provider}, default)")
 	void insertSecurityUser(SecurityUser securityUser);
 	
-	@Update("update securityUser set pwd=#{pwd}, uname=#{uname}, email+{email},"
+	@Update("update securityUser set pwd=#{pwd}, uname=#{uname}, email=#{email},"
 			+ " picture=#{picture} where uid=#{uid}")
 	void updateSecurityUser(SecurityUser securityUser);
 	
--- src/main/java/com/example/springSecurity/service/MyOAuth2UserService.java
+++ src/main/java/com/example/springSecurity/service/MyOAuth2UserService.java
@@ -1,5 +1,7 @@
 package com.example.springSecurity.service;
 
+import java.util.Map;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -32,28 +34,80 @@ public class MyOAuth2UserService extends DefaultOAuth2UserService{
 		String provider = userRequest.getClientRegistration().getRegistrationId();
 		switch (provider) {
 		case "google":
-			break;
+			String gid = oAuth2User.getAttribute("sub");
+			uid = provider + "_" + gid;
+			securityUser = securityService.getUserByUid(uid);
+			if (securityUser == null) {		// 가입이 되어있지 않으므로 가입 진행
+				uname = oAuth2User.getAttribute("name");
+				uname = (uname == null) ? "google_user" : uname;
+				email = oAuth2User.getAttribute("email");
+				picture = oAuth2User.getAttribute("picture");
+				securityUser = SecurityUser.builder()
+						.uid(uid).pwd(hashedPwd).uname(uname).email(email).picture(picture)
+						.provider(provider).build();
+				securityService.insertSecurityUser(securityUser);
+				securityUser = securityService.getUserByUid(uid);
+				log.info("구글 계정을 통해 회원가입이 됐다.");
+		}
+		break;
+			
 		case "github":
 			int id = oAuth2User.getAttribute("id");
 			uid = provider + "_" + id;
 			securityUser = securityService.getUserByUid(uid);
-		if (securityUser == null) {		// 가입이 되어있지 않으므로 가입 진행
-			uname = oAuth2User.getAttribute("name");
-			email = oAuth2User.getAttribute("email");
-			picture = oAuth2User.getAttribute("avatar_url");
-			securityUser = SecurityUser.builder()
-					.uid(uid).pwd(hashedPwd).uname(uname).email(email).picture(picture)
-					.provider(provider).build();
-			securityService.insertSecurityUser(securityUser);
-			securityUser = securityService.getUserByUid(uid);
-			log.info("깃허브 계정을 통해 회원가입이 됐다.");
-		}
+			if (securityUser == null) {		// 가입이 되어있지 않으므로 가입 진행
+				uname = oAuth2User.getAttribute("name");
+				uname = (uname == null) ? "github_user" : uname;
+				email = oAuth2User.getAttribute("email");
+				picture = oAuth2User.getAttribute("avatar_url");
+				securityUser = SecurityUser.builder()
+						.uid(uid).pwd(hashedPwd).uname(uname).email(email).picture(picture)
+						.provider(provider).build();
+				securityService.insertSecurityUser(securityUser);
+				securityUser = securityService.getUserByUid(uid);
+				log.info("깃허브 계정을 통해 회원가입이 됐다.");
+			}
 			break;
+		
 		case "naver":
+			Map<String, Object> response =(Map) oAuth2User.getAttribute("response");
+			String nid = (String) response.get("id");
+			uid = provider + "_" + nid;
+			securityUser = securityService.getUserByUid(uid);
+			if (securityUser == null) {		// 가입이 되어있지 않으므로 가입 진행
+				uname = (String) response.get("nickname");
+				uname = (uname == null) ? "naver_user" : uname;
+				email = (String) response.get("email");
+				picture = (String) response.get("profile_image");
+				securityUser = SecurityUser.builder()
+						.uid(uid).pwd(hashedPwd).uname(uname).email(email).picture(picture)
+						.provider(provider).build();
+				securityService.insertSecurityUser(securityUser);
+				securityUser = securityService.getUserByUid(uid);
+				log.info("네이버 계정을 통해 회원가입이 됐다.");
+			}
 			break;
+		
 		case "kakao":
+			long kid = (long) oAuth2User.getAttribute("id");
+			uid = provider + "_" + kid;
+			securityUser = securityService.getUserByUid(uid);
+			if (securityUser == null) {		// 가입이 되어있지 않으므로 가입 진행
+				Map<String, String> properties = (Map) oAuth2User.getAttribute("properties");
+				Map<String, Object> account = (Map) oAuth2User.getAttribute("kakao_account");
+				uname = (String) properties.get("nickname");
+				uname = (uname == null) ? "kakao_user" : uname;
+				email = (String) account.get("email");
+				picture = (String) properties.get("profile_image");
+				securityUser = SecurityUser.builder()
+						.uid(uid).pwd(hashedPwd).uname(uname).email(email).picture(picture)
+						.provider(provider).build();
+				securityService.insertSecurityUser(securityUser);
+				securityUser = securityService.getUserByUid(uid);
+				log.info("카카오 계정을 통해 회원가입이 됐다.");
+			}
 			break;
-		}
+	}
 		
 		
 		return new MyUserDetails(securityUser, oAuth2User.getAttributes());
